7d4fb6feb352877a714a9ba6162063d493f653d7
Szymon Hyziak <szymon.hyziak@gmail.com>
2022-07-17 17:24:41 +0200

    Changing the package's description

--- README.md
+++ README.md
@@ -6,42 +6,80 @@ A three module JavaScript library made by Szymon Hyziak
 
 Tools for working with the browser on the frontend
 
-### Functions 
+### setLocation()
 
-- setLocation()
-- refresh()
-- scrollTop()
-- goBack()
-- getSelection()
-- getCurrentPath()
-- integrateTabIndex()
-- getOffset()
-- getPrefferedTheme()
-- doesExist()
-- fadeIn()
-- fadeOut()
+Change the current page's URL
+
+### refresh()
+
+Reload the page
+
+### scrollTop()
+
+Scroll to the top of the page
+
+### goBack()
+
+Go to the previous page in the browser's history
+
+### getSelection()
+
+Get content of the clipboard
+
+### getCurrentPath()
+
+Get the page's path
+
+### integrateTabIndex()
+
+Make tabindex work on enter's click
+
+### getOffset()
+
+Get HTML element's position on X and Y axes
+
+### getPrefferedTheme()
+
+Get the theme preffered by the user
+
+### doesExist()
+
+Check if an element is present in an HTML document
+
+### fadeIn()
+
+Animate an HTML element in
+
+### fadeOut()
+
+Animate an HTML element out
 
 ## Module 2 (utilities)
 
 Common programming issues have been adressed in this part of the package
 
-### Functions
+### generateRandomColor()
 
-- generateRandomColor()
-- shuffleArray()
-- sleep()
-- generateRandomNumberInRange()
-- validateName()
-- reverseString()
-- getRandomElement()
-- getTime()
+### shuffleArray()
+
+### sleep()
+
+### generateRandomNumberInRange()
+
+### validateName()
+
+### reverseString()
+
+### getRandomElement()
+
+### getTime()
 
 ## Module 3: (easter-eggs)
 
 Making use of some funny JavaScript snippets
 
-### Functions
+### getFloatingPointNumber()
 
-- getFloatingPointNumber()
-- getBanana()
-- fail()
+### getBanana()
+
+### fail()
--- browser/index.js
+++ browser/index.js
@@ -1,3 +1,7 @@
+/**
+ * Change the current page's URL
+ * @param {string} location New URL  
+ */
 export const setLocation = (location) => {
     try {
         window.location.href = location;
@@ -6,6 +10,9 @@ export const setLocation = (location) => {
     }
 };
 
+/**
+ * Reload the page
+ */
 export const refresh = () => {
     try {
         window.location.reload(true);
@@ -14,6 +21,9 @@ export const refresh = () => {
     }
 }
 
+/**
+ * Scroll to the top of the page
+ */
 export const scrollTop = () => {
     try {
         window.scrollTo(0, 0);
@@ -22,6 +32,9 @@ export const scrollTop = () => {
     }
 };
 
+/**
+ * Go to the previous page in the browser's history
+ */
 export const goBack = () => {
     try {
         history.back();
@@ -30,6 +43,10 @@ export const goBack = () => {
     }
 }
 
+/**
+ * Get cntent of the clipboard
+ * @returns {string} Currently selected content
+ */
 export const getSelection = () => {
     try {
         return window.getSelection().toString();
@@ -38,6 +55,10 @@ export const getSelection = () => {
     }
 }
 
+/**
+ * Get the page's path
+ * @returns {string} Current URL path
+ */
 export const getCurrentPath = () => {
     try {
         return window.location.pathname;
@@ -46,6 +67,9 @@ export const getCurrentPath = () => {
     }
 }
 
+/**
+ * Make tabindex work on enter click
+ */
 export const useTabIndex = () => {
     try {
         document.addEventListener("keydown", (event) => {
@@ -56,6 +80,10 @@ export const useTabIndex = () => {
     }
 }
 
+/**
+ * Get the theme preffered by the user
+ * @returns {string} User chosen theme name
+ */
 export const getPrefferedTheme = () => {
     try {
         return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
@@ -64,6 +92,11 @@ export const getPrefferedTheme = () => {
     }
 }
 
+/**
+ * Get HTML element's position on X and Y axes
+ * @param {*} element HTML element 
+ * @returns {object} Element's position on a page
+ */
 export const getOffset = (element) => {
     try {
         const box = element.getBoundingClientRect();
@@ -76,6 +109,11 @@ export const getOffset = (element) => {
     }
 }
 
+/**
+ * Check if an element is present in an HTML document
+ * @param {*} element HTML element
+ * @returns {boolean} If the element is present on the page or not
+ */
 export const doesExist = (element) => {
     try {
         if (typeof (element) !== 'undefined' && element != null) return true;
@@ -85,6 +123,11 @@ export const doesExist = (element) => {
     }
 }
 
+/**
+ * Animate an HTML element in
+ * @param {*} element An HTML element
+ * @param {string} ms Time of the animation
+ */
 export const fadeOut = (element, ms) => {
     try {
         if (!ms) return;
@@ -96,6 +139,11 @@ export const fadeOut = (element, ms) => {
     }
 }
 
+/**
+ * Animate an HTML element out
+ * @param {*} element An HTML element
+ * @param {string} ms Time of the animation
+ */
 export const fadeIn = (element, ms) => {
     try {
         element.style.opacity = 0;
@@ -116,4 +164,3 @@ export const fadeIn = (element, ms) => {
         console.error(error);
     }
 }
-
