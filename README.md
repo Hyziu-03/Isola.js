842f19feae77a3afd4771984b49c875973a2b1d8
Szymon Hyziak <szymon.hyziak@gmail.com>
2022-07-17 17:01:44 +0200

    Publishing the package

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
 # Isola
  
-A JavaScript library made by Szymon Hyziak
+A three module JavaScript library made by Szymon Hyziak
+
+## Module 1: browser
+
+### Functions 
+
+- setLocation()
+- refresh()
+- scrollTop()
+- goBack()
+- getSelection()
+- getCurrentPath()
+- integrateTabIndex()
+- getOffset()
+- getPrefferedTheme()
+- doesExist()
+- fadeIn()
+- fadeOut()
+
+## Module 2: utilities
+
+### Functions
+
+- generateRandomColor()
+- shuffleArray()
+- sleep()
+- generateRandomNumberInRange()
+- validateName()
+- reverseString()
+- getRandomElement()
+- getTime()
+
+### Module 3: easter-eggs
+
+### Functions
+
+- getFloatingPointNumber()
+- getBanana()
+- fail()
--- browser/index.js
+++ browser/index.js
@@ -63,3 +63,57 @@ export const getPrefferedTheme = () => {
         console.error(error);
     }
 }
+
+export const getOffset = (element) => {
+    try {
+        const box = element.getBoundingClientRect();
+        return {
+            top: box.top + window.pageYOffset - document.documentElement.clientTop,
+            left: box.left + window.pageXOffset - document.documentElement.clientLeft
+        };
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const doesExist = (element) => {
+    try {
+        if (typeof (element) !== 'undefined' && element != null) return true;
+        else return false;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const fadeOut = (element, ms) => {
+    try {
+        if (!ms) return;
+        element.style.transition = `opacity ${ms} ms`;
+        element.addEventListener('transitionend', () => element.style.display = 'none', false);
+        element.style.opacity = '0';
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const fadeIn = (element, ms) => {
+    try {
+        element.style.opacity = 0;
+        if (!ms) {
+            element.style.opacity = 1;
+            return;
+        }
+        let opacity = 0;
+        const timer = setInterval(() => {
+            opacity += 50 / ms;
+            if (opacity >= 1) {
+                clearInterval(timer);
+                opacity = 1;
+            }
+            element.style.opacity = opacity;
+        }, 50);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
